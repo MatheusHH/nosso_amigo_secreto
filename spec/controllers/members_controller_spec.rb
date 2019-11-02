437ba55285a8193a18fa6319dd2daffd0eb4a7d7
Matheus Henrique <matheussilva770@gmail.com>
2019-11-02 14:37:36 -0300

    tests members passing, update, delete and destroy

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,19 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe CampaignsController, type: :controller do
+RSpec.describe MembersController, type: :controller do
   include Devise::Test::ControllerHelpers
 
   before(:each) do
     @request.env["devise.mapping"] = Devise.mappings[:user]
     @current_user = FactoryBot.create(:user)
     sign_in @current_user
-  end
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
   end
 
   describe "POST #create" do
@@ -51,30 +44,29 @@ RSpec.describe CampaignsController, type: :controller do
 
   end
 
-  #describe "PUT #update" do
-   # before(:each) do
-    #  @new_member_attributes = attributes_for(:member)
-     # request.env["HTTP_ACCEPT"] = 'application/json'
-    #end
+  describe "PUT #update" do
+    before(:each) do
+      @new_member_attributes = attributes_for(:member)
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
 
-    #context "Accepted new params " do
-     # before(:each) do
-      #  campaign = create(:campaign, user: @current_user)
-       # member = create(:member, campaign_id: campaign.id)
-       # put :update, params: {id: member.id, member: @new_member_attributes}
-      #end
+    context "Accepted new params " do
+      before(:each) do
+        campaign = create(:campaign, user: @current_user)
+        member = create(:member, campaign_id: campaign.id)
+        put :update, params: {id: member.id, member: @new_member_attributes}
+      end
 
-      #it "returns http success" do
-        #expect(response).to have_http_status(:success)
-      #end
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
 
-      #it "Member has the new attributes" do
-      #  expect(Member.last.name).to eq(@new_member_attributes[:name])
-      #  expect(Member.last.email).to eq(@new_member_attributes[:email])
-      #  expect(Member.last.campaign.id).to eq(@new_member_attributes[:campaign_id])
-      #end
-    #end
-  #end
+      it "Member has the new attributes" do
+        expect(Member.last.name).to eq(@new_member_attributes[:name])
+        expect(Member.last.email).to eq(@new_member_attributes[:email])
+      end
+    end
+  end
 
 
 end
